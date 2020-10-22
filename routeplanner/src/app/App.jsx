3d522459249df212072aa2750fc960a1ae45574f
mistervaibhav <vebatlow2@gmail.com>
2020-10-22 16:46:54 +0530

    :sparkles: display data to table added

--- routeplanner/src/app/App.jsx
+++ routeplanner/src/app/App.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map, TileLayer } from 'react-leaflet';
 
-import { Button } from 'antd';
-import { Upload, message } from 'antd';
+import { Button, Space, Upload, Table } from 'antd';
 
 import { InboxOutlined } from '@ant-design/icons';
 
@@ -17,21 +16,89 @@ const App = () => {
   const { Dragger } = Upload;
 
   const [position, setPosition] = useState([28.7041, 77.1025]);
+  const [file, setFile] = useState({});
+  const [fileList, setFileList] = useState([]);
   const [locations, setLocations] = useState([]);
+  const [isTableDataPresent, setIsTableDataPresent] = useState(false);
+  const [isTableVisible, setIsTableVisible] = useState(false);
+  const [tableColumns, setTableColumns] = useState([]);
+  const [tableData, setTableData] = useState([]);
 
-  function fileUploadHandler(event) {
-    console.log(event);
+  useEffect(() => {
+    populateTable(locations);
+  }, [locations]);
 
-    const { file } = event;
-    const reader = new FileReader();
-
-    if (file.status !== 'removed') {
+  useEffect(() => {
+    if (Object.keys(file).length === 0 && file.constructor === Object) {
+      // console.log('file is empty');
+      setIsTableDataPresent(false);
+      setLocations([]);
+    } else {
+      const reader = new FileReader();
       reader.readAsText(file);
-
       reader.onload = () => {
         setLocations(csvToJson(reader.result));
       };
+      setIsTableDataPresent(true);
     }
+  }, [file]);
+
+  useEffect(() => {
+    if (fileList.length === 1) {
+      setFile(fileList[0]['originFileObj']);
+      console.log('filelist modified');
+    } else {
+      setIsTableDataPresent(false);
+      setLocations([]);
+    }
+  }, [fileList]);
+
+  function fileUploadHandler(event) {
+    let fileList = [...event.fileList];
+
+    fileList = fileList.slice(-1);
+
+    setFileList(fileList);
+  }
+
+  function toggleTableVisibility() {
+    if (isTableVisible) {
+      setIsTableVisible(false);
+    } else {
+      setIsTableVisible(true);
+    }
+  }
+
+  function populateTable(locations) {
+    if (locations.length > 1) {
+      let columns = Object.keys(locations[0]).map((item) => ({
+        title: item,
+        dataIndex: camelize(item),
+        key: camelize(item),
+      }));
+
+      let data = locations.map((item, index) => {
+        let row = {};
+
+        for (let key of Object.keys(item)) {
+          row[camelize(key)] = item[key];
+        }
+
+        row['key'] = index + 1;
+        return row;
+      });
+
+      setTableColumns(columns);
+      setTableData(data);
+    }
+  }
+
+  function camelize(str) {
+    return str
+      .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+        return index === 0 ? word.toLowerCase() : word.toUpperCase();
+      })
+      .replace(/\s+/g, '');
   }
 
   return (
@@ -48,8 +115,8 @@ const App = () => {
             name='file'
             onChange={(event) => fileUploadHandler(event)}
             beforeUpload={() => false}
-            multiple={false}
             accept='.csv'
+            fileList={fileList}
           >
             <p className='ant-upload-drag-icon'>
               <InboxOutlined />
@@ -61,7 +128,26 @@ const App = () => {
             </p>
           </Dragger>
         </div>
-        <div className='right'></div>
+        <div className='right'>
+          <Space>
+            <Button
+              type='primary'
+              disabled={!isTableDataPresent ? true : false}
+              onClick={() => toggleTableVisibility()}
+            >
+              {isTableVisible ? 'Hide' : 'Show'} table
+            </Button>
+            <Button type='primary' disabled={!isTableDataPresent ? true : false}>
+              Display markers
+            </Button>
+            <Button type='primary' disabled={!isTableDataPresent ? true : false}>
+              Create Route
+            </Button>
+          </Space>
+          {isTableVisible && isTableDataPresent ? (
+            <Table columns={tableColumns} dataSource={tableData} />
+          ) : null}
+        </div>
       </div>
     </div>
   );
